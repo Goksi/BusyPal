a61dafec8f25303ead451c814f15ba655b50c594
Vukašin Lekić <goksi1312@gmail.com>
2025-09-17 19:20:33 +0200

    add checkstyle

--- src/main/java/tech/goksi/busypal/BusyPalApplication.java
+++ src/main/java/tech/goksi/busypal/BusyPalApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BusyPalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BusyPalApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(BusyPalApplication.class, args);
+  }
 
 }
