3428a17bbc3b4e5f2c93b953a83e6bb974e4c7c7
Vukašin Lekić <goksi1312@gmail.com>
2025-09-20 01:29:43 +0200

    Add dynamic QR code generation with loading spinner

--- src/main/resources/static/js/qr-handler.js
+++ src/main/resources/static/js/qr-handler.js
@@ -6,22 +6,38 @@ function connect() {
     console.log('Connected to ws: ' + frame);
     let session = getCookie('busypal_session')
     stompClient.subscribe('/topic/qr-user' + session, handleMessage);
-    stompClient.publish({destination: '/busypal/ready', body: JSON.stringify({event_type: "client_ready", cookie: session})})
+    stompClient.publish({
+      destination: '/busypal/ready',
+      body: JSON.stringify({event_type: "client_ready", cookie: session})
+    })
   });
 }
 
 function getCookie(name) {
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop().split(';').shift();
+  if (parts.length === 2) {
+    return parts.pop().split(';').shift();
+  }
 }
-
 
 function handleMessage(message) {
   let parsedMsg = JSON.parse(message.body);
   if (parsedMsg.event_type === 'wa_qr_code') {
-    let qrCode = parsedMsg.qr;
-    console.log("DOBIO SAM QR: " + qrCode)
+    let qrCodeText = parsedMsg.qr;
+    console.log("DOBIO SAM QR: " + qrCodeText);
+    let qrElement = document.getElementById('qr');
+    qrElement.classList.remove('qr-code-spinning');
+    qrElement.classList.add('qr-code');
+    document.getElementById('spinner').remove();
+    new QRCode(qrElement, {
+      text: qrCodeText,
+      width: 200,
+      height: 200,
+      colorDark: `#000`,
+      colorLight: `#fff`,
+      correctLevel: QRCode.CorrectLevel.L
+    });
   }
 }
 
