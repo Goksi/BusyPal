3e84c8da21795d28e3202719803d421432eed578
Vukašin Lekić <goksi1312@gmail.com>
2025-09-21 00:01:46 +0200

    add log statement

--- src/main/java/tech/goksi/busypal/event/listener/SessionFixationListener.java
+++ src/main/java/tech/goksi/busypal/event/listener/SessionFixationListener.java
@@ -1,13 +1,18 @@
 package tech.goksi.busypal.event.listener;
 
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationListener;
 import org.springframework.security.web.authentication.session.SessionFixationProtectionEvent;
 import org.springframework.stereotype.Component;
 import tech.goksi.busypal.manager.WhatsAppManager;
 
 @Component
-public class SessionFixationListener implements ApplicationListener<SessionFixationProtectionEvent> {
+public class SessionFixationListener implements
+    ApplicationListener<SessionFixationProtectionEvent> {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(SessionFixationListener.class);
 
   private final WhatsAppManager manager;
 
@@ -17,6 +22,8 @@ public class SessionFixationListener implements ApplicationListener<SessionFixat
 
   @Override
   public void onApplicationEvent(@NotNull SessionFixationProtectionEvent event) {
+    LOGGER.debug("Session fixation protection for id {} occurred, migrating whatsapp session...",
+        event.getOldSessionId());
     manager.migrateSession(event.getOldSessionId(), event.getNewSessionId());
   }
 }
