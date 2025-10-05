b25c9d3cde94cfdb9d8174c029ae7cd9b3960360
Vukašin Lekić <goksi1312@gmail.com>
2025-10-06 01:23:39 +0200

    rename advice & add advice for session id

--- src/main/java/tech/goksi/busypal/advice/GlobalModelAttributesAdvice.java
+++ src/main/java/tech/goksi/busypal/advice/GlobalModelAttributesAdvice.java
@@ -1,12 +1,14 @@
 package tech.goksi.busypal.advice;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import tech.goksi.busypal.security.model.WhatsAppPrincipal;
 
 @ControllerAdvice
-public class CurrentUserPhoneAdvice {
+public class GlobalModelAttributesAdvice {
 
   @ModelAttribute("currentPhone")
   public String phoneNumber(Authentication authentication) {
@@ -18,5 +20,11 @@ public class CurrentUserPhoneAdvice {
       return phoneNumber;
     }
     return null;
+  }
+
+  @ModelAttribute("sessionId")
+  public String sessionId(HttpServletRequest request) {
+    HttpSession session = request.getSession(false);
+    return session != null ? session.getId() : "";
   }
 }
