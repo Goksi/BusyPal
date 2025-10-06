b0eddbd7240769e11ece3494c401f5e102335a15
Vukašin Lekić <goksi1312@gmail.com>
2025-10-06 16:56:12 +0200

    Update BusyManager.java

--- src/main/java/tech/goksi/busypal/manager/BusyManager.java
+++ src/main/java/tech/goksi/busypal/manager/BusyManager.java
@@ -2,14 +2,6 @@ package tech.goksi.busypal.manager;
 
 public interface BusyManager {
 
-  default void setBusy(String sessionId) {
-    setBusy(sessionId, true);
-  }
-
-  default void setNotBusy(String sessionId) {
-    setBusy(sessionId, false);
-  }
-
   boolean isBusy(String sessionId);
 
   void setBusy(String sessionId, boolean busy);
